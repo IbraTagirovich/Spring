a910968af3f0b9ace41443ffa670b85c93d324d8
Ibragimhalil Kurbanov <149794271+IbraTagirovich@users.noreply.github.com>
2026-04-23 21:17:00 +0300

    Update Egg6.java

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,20 +1,9 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Egg6 {
-
-    private Needle7 needle;
-
-    @Autowired          // связывание через сеттер
-    public void setNeedle(Needle7 needle) {
-        this.needle = needle;
-    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + needle.toString();
+        return ", в яйце иголка " + "";
     }
 }

e734b53c65fe660990340edf3df04ecae0e2ba3f
IbraTagirovich <www.arteswin86@gmail.com>
2026-04-23 23:21:35 +0300

    First commit 2.2.1 Решение

--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,10 +1,11 @@
 package hiber.service;
 
 import hiber.model.User;
-
 import java.util.List;
 
 public interface UserService {
     void add(User user);
     List<User> listUsers();
+    User getUserByCarModelAndSeries(String model, int series); //добавил новый метод для того чтобы hql запросом доставать юзера,
+                                                               //который владеет машиной по модели и по серии
 }
